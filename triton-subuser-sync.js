3b3e37d394df98905b9b0c174b7bdd421eda4b9f
Jeff Goeke-Smith <goekesmi@msu.edu>
2019-04-26 21:41:11 +0000

    subuser sync and a tool to empty out all subusers

--- triton-subuser-sync.js
+++ triton-subuser-sync.js
@@ -1,15 +1,10 @@
 #!/bin/env node
 
-const Gitlab = require("gitlab/dist/es5").default;
 var fs = require("fs");
 
 var config = require("./mantaconfig.json");
 
-var gitlabconfig = require("./gitlabconfig.json");
-
-var mantakeys = require("./mantablob.json");
-
-console.log(mantakeys);
+var mantakeys = require("./subuserblob.json");
 
 var fs = require("fs");
 var smartdc = require("smartdc");
@@ -24,7 +19,6 @@ var client = smartdc.createClient({
   url: process.env.SDC_URL
 });
 
-const api = new Gitlab(gitlabconfig);
 
 // List all users, delete users that don't exist in my config
 client.listUsers(function(err, users) {
@@ -35,7 +29,7 @@ client.listUsers(function(err, users) {
 
   users.forEach(function(m) {
     console.log("User: " + JSON.stringify(m, null, 2));
-    console.log(mantakeys.subuserkeys[m.login]);
+    //console.log(mantakeys.subuserkeys[m.login]);
     if (typeof mantakeys.subuserkeys[m.login] == "undefined") {
       console.log("deleting user " + m.login);
       client.deleteUser(m, function(err) {
@@ -48,14 +42,15 @@ client.listUsers(function(err, users) {
     }
   });
 });
+
 // Add all users that don't exist in my configuration
-mantakeys.users.forEach(function(m) {
+Object.keys(mantakeys.subuserkeys).forEach(function(m) {
   client.getUser(m, function(err, user) {
     if (err) {
       console.log("err: " + err);
       console.log(err.stringify);
       if (String(err).lastIndexOf("ResourceNotFoundError", 0) == 0) {
-        console.log("I should create it.");
+        console.log("User" + m + " not found. Attempting to create.");
         client.createUser(
           { login: m, password: "none123$%^", email: m + "@msu.edu" },
           function(err) {
@@ -63,7 +58,7 @@ mantakeys.users.forEach(function(m) {
               console.log("Unable to create user: " + err);
               return;
             }
-            console.log("User created. ");
+            console.log("User " + m +" created. ");
           }
         );
       }
@@ -73,24 +68,23 @@ mantakeys.users.forEach(function(m) {
   });
 });
 // For each user delete keys that don't exist in my configuration
-mantakeys.users.forEach(function(m) {
+Object.keys(mantakeys.subuserkeys).forEach(function(m) {
   client.listUserKeys(m, function(err, keys) {
     if (err) {
       console.log("err: " + err);
       return;
     }
-    console.log(keys);
+    //console.log(m);
   });
 });
 
 // For each user add keys that exist in my configuration
-mantakeys.users.forEach(function(m) {
+Object.keys(mantakeys.subuserkeys).forEach(function(m) {
   client.getUser(m, function(err, user) {
     if (err) {
       console.log("err: " + err);
       return;
     }
-    console.log(mantakeys.subuserkeys);
     mantakeys.subuserkeys[m].keys.forEach(function(key) {
       client.uploadUserKey(user, { key: key }, function(err, key) {
         if (err) {
